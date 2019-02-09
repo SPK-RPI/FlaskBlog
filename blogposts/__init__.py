0ae5bb79e543ba3516b917ca47ec5a09d69bb7ff
SPK-RPI <shibaprasadkar50@gmail.com>
2019-02-09 23:40:57 +0530

    moreLogic

--- blogposts/__init__.py
+++ blogposts/__init__.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '989898D9F89S8989SF8989SMFDS98FDSFSLIFDI'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-bcript=Bcrypt(app)
+bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
+login_manager.login_view='login'
+login_manager.login_message_category='info'
 from blogposts import routes
--- blogposts/forms.py
+++ blogposts/forms.py
@@ -1,32 +1,35 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField,ValidationError
-from blogposts.models import User 
-from wtforms.validators import DataRequired, Length, Email, equal_to
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from blogposts.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[
-        DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    conform_password = PasswordField('Conform Password', validators=[
-                                     DataRequired(), equal_to('password')])
-    submit = SubmitField('Sign up')
-    
-    def validate_username(self,username):
-            user=User.query.filter_by(username=username.data).first()
-            if user:
-                    raise ValidationError('That user name has been taken. Use a different one')
-    
-    def validate_email(self,email):
-            user=User.query.filter_by(email=email.data).first()
-            if user:
-                    raise ValidationError('That Email has been taken. Use a different one')                
+    conform_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                'That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                'That email is taken. Please choose a different one.')
+
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[
-        DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit = SubmitField('LogIn')
+    submit = SubmitField('Login')
